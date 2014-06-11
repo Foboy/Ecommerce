275413ac1c46fcbead47d4065a234ffeca875768
kbtyc <66954011@qq.com>
2014-06-11 20:44:16 +0800

    11

---
+++ RuiCui/Plugins/Nop.Plugin.Widgets.NivoSlider/Models/PublicInfoIndexSliderRootModel.cs
@@ -0,0 +1,20 @@
+﻿using Nop.Web.Framework.Mvc;
+
+namespace Nop.Plugin.Widgets.NivoSlider.Models
+{
+    public class PublicInfoIndexSliderRootModel : BaseNopModel
+    {
+        public string Picture1Url { get; set; }
+        public string Text1 { get; set; }
+        public string Link1 { get; set; }
+
+        public string Picture2Url { get; set; }
+        public string Text2 { get; set; }
+        public string Link2 { get; set; }
+
+        public string Picture3Url { get; set; }
+        public string Text3 { get; set; }
+        public string Link3 { get; set; }
+
+    }
+}
