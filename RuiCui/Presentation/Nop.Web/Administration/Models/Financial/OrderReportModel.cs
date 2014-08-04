0d81d4fe191b78e0f546173934fa71fd019cf6de
kbtyc <66954011@qq.com>
2014-08-04 11:40:58 +0800

    财务结算

---
+++ RuiCui/Presentation/Nop.Web/Administration/Models/Financial/OrderReportModel.cs
@@ -0,0 +1,21 @@
+﻿using Nop.Web.Framework;
+using Nop.Web.Framework.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Nop.Admin.Models.Orders
+{
+    public partial class OrderReportModel : BaseNopModel
+    {
+
+
+        [NopResourceDisplayName("Admin.Orders.List.OrderStatus")]
+        public int OrderStatusId { get; set; }
+
+        [UIHint("DateNullable")]
+        public DateTime? StartDate { get; set; }
+
+        [UIHint("DateNullable")]
+        public DateTime? EndDate { get; set; }
+    }
+}
